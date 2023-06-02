3722d22c6692c0cb3ab838a8b5bb73084bf62903
aslanukr <114025378+aslanukr@users.noreply.github.com>
2023-06-02 16:07:27 +0000

    Deploying to gh-pages from @ aslanukr/goit-react-hw-05-movies@ab6643ef50e9c5774c550f3432cf2c1ffd2b05ba 🚀

---
+++ static/js/856.25110a3a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[856],{6856:function(t,e,a){a.d(e,{KYK:function(){return n}});var r=a(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}}]})(t)}}}]);
+//# sourceMappingURL=856.25110a3a.chunk.js.map
